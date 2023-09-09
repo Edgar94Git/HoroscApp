f6fda2ae449180c2c2281632f8ff0ffa0643b91c
Edgar Reyes <gzzenit@gmail.com>
2023-09-08 20:13:37 -0600

    fragment is added and view binding is add

---
+++ app/src/main/java/com/ereyes/horoscapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,23 @@
+package com.ereyes.horoscapp.ui.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ereyes.horoscapp.R
+import com.ereyes.horoscapp.databinding.FragmentLuckBinding
+
+class LuckFragment : Fragment() {
+
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLuckBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
