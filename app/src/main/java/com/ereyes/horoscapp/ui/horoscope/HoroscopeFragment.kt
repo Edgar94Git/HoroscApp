c56e03caa21ba91505b565ed0e29498322663e62
Edgar Reyes <gzzenit@gmail.com>
2023-09-09 16:21:41 -0600

    update fragment

--- app/src/main/java/com/ereyes/horoscapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/ereyes/horoscapp/ui/horoscope/HoroscopeFragment.kt
@@ -1,19 +1,62 @@
 package com.ereyes.horoscapp.ui.horoscope
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.ereyes.horoscapp.R
 import com.ereyes.horoscapp.databinding.FragmentHoroscopeBinding
+import com.ereyes.horoscapp.ui.horoscope.adapter.HoroscopeAdapter
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
 
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
+    private val horoscopeViewModel by viewModels<HoroscopeViewModel>()
+    private lateinit var horoscopeAdapter: HoroscopeAdapter
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIState()
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        horoscopeAdapter = HoroscopeAdapter(onItemSelected = { horoscopeInfo ->
+            Snackbar.make(binding.root, horoscopeInfo.name, Snackbar.LENGTH_LONG).show()
+        })
+        binding.rvHoroscopeList.apply {
+            layoutManager = GridLayoutManager(context, 2)
+            adapter = horoscopeAdapter
+        }
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeViewModel.horoscope.collect { horoscopeList ->
+                    horoscopeAdapter.updateList(horoscopeList)
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
