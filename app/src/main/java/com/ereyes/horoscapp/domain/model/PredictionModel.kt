fc06d721bf8f77a0df241f1ff42ee13790ace595
Edgar Reyes <gzzenit@gmail.com>
2023-09-11 08:01:44 -0600

    PredictionModel is added

---
+++ app/src/main/java/com/ereyes/horoscapp/domain/model/PredictionModel.kt
@@ -0,0 +1,12 @@
+package com.ereyes.horoscapp.domain.model
+
+/****
+ * Project: HoroscApp
+ * From: com.ereyes.horoscapp.domain.model
+ * Created by Edgar Reyes Gonzalez on 9/10/2023 at 3:24 PM
+ * All rights reserved 2023.
+ ****/
+data class PredictionModel(
+    val horoscope: String,
+    val sign: String
+)
