09250f9fd6bf27719153ece7053407e7af7c1a44
Edgar Reyes <gzzenit@gmail.com>
2023-09-11 08:01:20 -0600

    HoroscopeDetailState is added

---
+++ app/src/main/java/com/ereyes/horoscapp/ui/detail/HoroscopeDetailState.kt
@@ -0,0 +1,15 @@
+package com.ereyes.horoscapp.ui.detail
+
+import com.ereyes.horoscapp.domain.model.PredictionModel
+
+/****
+ * Project: HoroscApp
+ * From: com.ereyes.horoscapp.ui.detail
+ * Created by Edgar Reyes Gonzalez on 9/10/2023 at 1:39 PM
+ * All rights reserved 2023.
+ ****/
+sealed class HoroscopeDetailState {
+    data object Loading: HoroscopeDetailState()
+    data class Error(val error:String): HoroscopeDetailState()
+    data class Success(val prediction: PredictionModel): HoroscopeDetailState()
+}
